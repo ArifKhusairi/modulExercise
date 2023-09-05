79d3f1c9eccd9ba4ce7ed8f89757071c7bd37e88
Adxmn <adamdanixl@gmail.com>
2023-09-05 11:44:44 +0800

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+import mymodule
+import arifModul
+from modulShamim import person1
+
+
+def main():
+    arifModul.displayName("Arif")
+    mymodule.wassap()
+    print(person1["age"])
+    print(person1["Country"])
+
+
+main()
